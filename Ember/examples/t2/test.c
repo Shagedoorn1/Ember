29328c598e97d5c09455fa4ea63cf984c4ddff1e
Shagedoorn1 <23066776@student.hhs.nl>
2025-07-26 18:00:41 +0200

    Update on: AmitX version v0.2.1; Owly version v2.0.0

--- Ember/examples/t2/test.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("\x1b[31mHello world from ember!\x1b[0m\n");
-}
--- Ember/include/parser.h
+++
@@ -1,8 +0,0 @@
-#ifndef PARSER_H
-#define PARSER_H
-
-#include "ember.h"
-
-int parse_emberfile(const char* filename, EmberBuildConfig* config);
-
-#endif
